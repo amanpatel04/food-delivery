9905fa45ff919a1db8d3fc6052a25a6edca7a656
Gaurav Bhatt <gbhatt570@gmail.com>
2026-03-26 21:47:45 +0530

    feat(customer): update controller and service logic
    # Conflicts:
    #	src/main/java/com/foodservice/controller/CustomerController.java
    #	src/main/java/com/foodservice/service/CustomerService.java

--- src/main/java/com/foodservice/service/CustomerService.java
+++ src/main/java/com/foodservice/service/CustomerService.java
@@ -1,10 +1,4 @@
 package com.foodservice.service;
 
-import com.foodservice.entity.dto.CustomerDTO;
-
-import java.util.List;
-
 public interface CustomerService {
-    List<CustomerDTO> getAllCustomers();
-    CustomerDTO getCustomerById(Long id);
 }

24334a7e88288367a0ec20e5b1c2b68d248cf13c
Aman Patel <65351629+amanpatel04@users.noreply.github.com>
2026-03-26 14:32:41 +0530

    Create Order and OrderItem entities with DTOs and repository fixes (#5)
    * feat(Order): created entity for entity Order and OrderItem
    
    - Custom table name and column name according to database
    - Lombok annotation for getter and setter
    - Repository file is renamed due to typo
    
    * (feat): created Data Transfer Object for strict response structure
    
    - ResponseDTO to send response object in proper structure
    - ErrorResponseDTO to send exception in proper structure
    - OrderDTO for Order entity
    - OrderItemDTO for OrderItem entity

---
+++ src/main/java/com/foodservice/entity/dto/ErrorResponseDTO.java
@@ -0,0 +1,19 @@
+package com.foodservice.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponseDTO {
+    private String apiPath;
+    private HttpStatus errorCode;
+    private String errorMessage;
+    private LocalDateTime timestamp;
+}
+
+
---
+++ src/main/java/com/foodservice/entity/dto/ResponseDTO.java
@@ -0,0 +1,14 @@
+package com.foodservice.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDTO {
+    private Integer statusCode;
+    private String message;
+    private Object data;
+}

fbd8f86227e8d50ecc75e5b89ec28f7d6edab64a
Samar Pratap Singh Wats <149253666+SamarWats@users.noreply.github.com>
2026-03-28 14:56:53 +0530

    implemented the get api's of relation between the driver and order

--- src/main/java/com/foodservice/controller/DeliveryDriverController.java
+++ src/main/java/com/foodservice/controller/DeliveryDriverController.java
@@ -4,31 +4,34 @@ import com.foodservice.entity.dto.DeliveryDriverResponseDTO;
 import com.foodservice.service.DeliveryDriverService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/drivers")
 @RequiredArgsConstructor
 public class DeliveryDriverController {
+	
+	
+    private DeliveryDriverService deliveryDriverservice ;
 
-    private final DeliveryDriverService service;
-    
-    public DeliveryDriverController(DeliveryDriverService service) {
-        this.service = service;
+    // ------- Get Driver by ID ----------
+    @GetMapping("/{driverId}")
+    public ResponseEntity<DeliveryDriverResponseDTO> getDriverById(@PathVariable Long driverId) {
+        return ResponseEntity.ok(deliveryDriverservice.getDriverById(driverId));
     }
 
-	
-	@GetMapping("{/driverId")
-	public DeliveryDriverResponseDTO getDriverById(@PathVariable long driverId) {
-		return service.getDriverById(driverId);
-	}
-	
-	
-	@GetMapping
-    public List<DeliveryDriverResponseDTO> getAllDrivers() {
-        return service.getAllDrivers();
+    // ----- Get All Drivers ----------
+    @GetMapping
+    public ResponseEntity<List<DeliveryDriverResponseDTO>> getAllDrivers() {
+        return ResponseEntity.ok(deliveryDriverservice.getAllDrivers());
     }
-	
+
+    //------- Get Driver Deliveries ----------
+    @GetMapping("/{driverId}/deliveries")
+    public ResponseEntity<List<DeliveryDriverResponseDTO>> getDriverDeliveries(@PathVariable Long driverId) {
+        return ResponseEntity.ok(deliveryDriverservice.getDriverDeliveries(driverId));
+    }
 }

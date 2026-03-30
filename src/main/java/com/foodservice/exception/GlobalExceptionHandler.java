d5a54348affe419fef79b2b5d4eb025359393b60
Aman Patel <65351629+amanpatel04@users.noreply.github.com>
2026-03-30 11:04:02 +0530

    Add Lombok Slf4j annotation to GlobalExceptionHandler

--- src/main/java/com/foodservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/foodservice/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import lombok.extern.slf4j.Slf4j
 
 import java.time.LocalDateTime;
 
